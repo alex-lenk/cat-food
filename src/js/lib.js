93bf68b58de51dfb08b5ebefa530acf115415746
Alex Lenk <itdev11@skdo.ru>
2018-06-27 18:04:50 +0300

    Initial commit

---
+++ src/js/lib.js
@@ -0,0 +1,7 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+// = lib/svgxuse.min.js
+
+// = lib/smoothscroll.js
+// = lib/bootstrap.min.js
+// = ../../bower_components/slick-carousel/slick/slick.min.js
