651318a9ad3a1e0b61f02bd3f71de737f5251f8c
alex-lenk <artlenk.ru@gmail.com>
2018-06-29 17:39:03 +0300

    Added answers

--- src/js/common.js
+++ src/js/common.js
@@ -1,16 +1,6 @@
 $(document).ready(function () {
-// Исправление бага в IE на телефонах
-// Copyright 2014-2017 The Bootstrap Authors
-// Copyright 2014-2017 Twitter, Inc.
-// Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
-        var msViewportStyle = document.createElement('style');
-        msViewportStyle.appendChild(
-            document.createTextNode(
-                '@-ms-viewport{width:auto!important}'
-            )
-        );
-        document.head.appendChild(msViewportStyle)
+    if (navigator.userAgent.match(/msie/i) || navigator.userAgent.match(/trident/i) ){
+        $("html").addClass("ie");
     }
 
     var listItemActive = 'list-item__active';
